fefbd9f0680d74687320df6dfd4f2d0e53643e7b
Juan Brusasca <juanbrusasca96@gmail.com>
2023-01-13 16:12:13 -0300

    server and utils

---
+++ api/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import productsRouter from './routes/products.routes.js'
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+
+app.use('/api/products', productsRouter)
---
+++ api/utils.js
@@ -0,0 +1,31 @@
+import config from "./config/config";
+import passport from "passport";
+import bcrypt from 'bcrypt';
+
+export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password);
+export const serialize = (object, keys) => {
+    let serializeObject = Object.fromEntries(Object.entries(object).filter(pair => keys.includes(pair[0])))
+    serializeObject.id = object._id;
+    return serializeObject;
+}
+export const cookieExtractor = req => {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies[config.jwt.cookie_name];
+    }
+    return token;
+}
+/*Middlewares*/
+export const passportCall = (strategy) => {
+    return async (req, res, next) => {
+        passport.authenticate(strategy, function (err, user, info) {
+            if (err) return next(err);
+            if (!user) {
+                return res.status(401).send({ error: info.messages ? info.messages : info.toString() });
+            }
+            req.user = user;
+            next();
+        })(req, res, next);
+    }
+}
