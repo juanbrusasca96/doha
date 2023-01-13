21a264d8ca0028a4b5875b8157933b3accdff0e1
Juan Brusasca <juanbrusasca96@gmail.com>
2023-01-13 14:35:56 -0300

    products routes

---
+++ api/routes/products.routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import productsController from '../controllers/products.controller.js';
+const router=express.Router();
+
+router.get('/',productsController.getAllProducts);
+router.post('/:pid',productsController.updateProduct);
+router.put('/:pid',productsController.deleteProduct);
+router.get('/:pid',productsController.getProductById);
+
+export default router;
