46e1ca103a2e3c80375059e0c3e73a45f369f70d
Juan Brusasca <juanbrusasca96@gmail.com>
2023-01-31 11:27:59 -0300

    first config

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+
+import dotenv from "dotenv";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_URL;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
