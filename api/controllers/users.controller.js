1ec107cd6cfa535fa1464956f15557126fc10b48
Juan Brusasca <juanbrusasca96@gmail.com>
2023-01-31 11:24:56 -0300

    change username to email

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -16,10 +16,10 @@ const getUserById = async (req, res) => {
 }
 
 const saveUser = async (req, res) => {
-    let { username, password } = req.body;
-    if (!username || !password) return res.status(400).send({ status: "error", error: "Imcomplete values" });
+    let { email, password } = req.body;
+    if (!email || !password) return res.status(400).send({ status: "error", error: "Imcomplete values" });
     await userService.save({
-        username,
+        email,
         password
     })
     res.send({ status: "success", message: "User added" })
