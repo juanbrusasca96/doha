87fa861c5775a95c858b23b9b7df09457a3c3a41
Juan Brusasca <juanbrusasca96@gmail.com>
2023-02-08 20:58:41 -0300

    utils

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,4 +1,10 @@
-export const categories = ['Vino', 'Espumante', 'Aperitivo', 'Cerveza', 'Gaseosa', 'Jugo', 'Energizante', 'Vodka', 'Ron', 'Gin', 'Licor', 'Whiskey', 'Tequila', 'Promo', 'Consumo en el local']
+export const Promo = 'Promo'
+
+export const categories = ['Vino', 'Espumante', 'Aperitivo', 'Cerveza', 'Gaseosa', 'Jugo', 'Energizante', 'Vodka', 'Ron', 'Gin', 'Licor', 'Whiskey', 'Tequila', Promo, 'Consumo en el local'];
+
+export const unitSizes=['ml', 'L', 'kg'];
+
+export const colors =['-', 'Tinto', 'Blanco', 'Rosado', 'Rubia', 'Negra', 'Roja']
 
 export function roundDecimals(number, decimals) {
     let numberRegexp = new RegExp('\\d\\.(\\d){' + decimals + ',}');    
