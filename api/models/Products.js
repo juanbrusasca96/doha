0e24565edcde44c288035392060f33c17d8a6132
Juan Brusasca <juanbrusasca96@gmail.com>
2023-02-11 03:46:10 -0300

    add default 0 to stock in product model

--- api/models/Products.js
+++ api/models/Products.js
@@ -11,7 +11,7 @@ const productSchema = new Schema({
     size: { type: String },
     unitSize: { type: String, enum: ['-', 'ml', 'L', 'kg'], default: '-' },
     image: { type: String, default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1GgAnJGA512_xkQEw7WimR_PDkeajIEG1_A&usqp=CAU' },
-    stock: { type: Number },
+    stock: { type: Number, default:0 },
     limitStock: { type: Number },
     category: { type: String, enum: ['Vino', 'Espumante', 'Aperitivo', 'Cerveza', 'Gaseosa', 'Jugo', 'Energizante', 'Vodka', 'Ron', 'Gin', 'Licor', 'Whiskey', 'Tequila', 'Promo', 'Consumo en el local'], required: true },
     color: { type: String, enum: ['-', 'Tinto', 'Blanco', 'Rosado', 'Rubia', 'Negra', 'Roja'], default: '-' },
