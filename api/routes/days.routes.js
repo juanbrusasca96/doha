78d6ce64d89ffd824db4aa32e7ce35b50af53083
Juan Brusasca <juanbrusasca96@gmail.com>
2023-02-15 13:36:16 -0300

    day routes

---
+++ api/routes/days.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import daysController from '../controllers/days.controller.js';
+const router = express.Router();
+
+router.get('/', daysController.getAllDays);
+router.post('/', daysController.saveDay);
+router.post('/:pid', daysController.updateDay);
+router.put('/:pid', daysController.deleteDay);
+router.get('/:pid', daysController.getDayById);
+
+export default router;
