5a42ebb514bfb0c16f4800242964c9a50145ce7d
Juan Brusasca <juanbrusasca96@gmail.com>
2023-03-02 13:13:50 -0300

    getAll products sort alphabetically

--- api/services/products.service.js
+++ api/services/products.service.js
@@ -5,4 +5,10 @@ export default class ProductService extends GenericQueries {
     constructor(dao) {
         super(dao, productModel);
     }
+
+    getAll = async () => {
+        if (!this.dao.models[this.model]) throw new Error(`Entity ${entity} not found or defined`)
+        let results = await this.dao.models[this.model].find().sort({name:'asc'});
+        return results.map(result => result.toObject())
+    }
 }
