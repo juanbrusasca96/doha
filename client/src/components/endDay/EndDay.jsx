1e71c84e1bd1b3d91a93d139f2f7ab2860dc910a
Juan Brusasca <juanbrusasca96@gmail.com>
2023-03-27 13:36:02 -0300

    end day basic functionality

--- client/src/components/endDay/EndDay.jsx
+++ client/src/components/endDay/EndDay.jsx
@@ -6,8 +6,17 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useDispatch, useSelector } from 'react-redux';
+import { endDay, getDay } from '../../redux/features/days/daysGetSlice';
 
-export default function EndDay({open, handleClose}) {
+export default function EndDay({ open, handleClose }) {
+  const dispatch = useDispatch();
+  const day = useSelector((state) => state.days.day)
+
+  const handleClick = () => {
+    dispatch(endDay(day._id, { ...day, active: false }))
+    handleClose()
+  }
 
   return (
     <div>
@@ -29,8 +38,8 @@ export default function EndDay({open, handleClose}) {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleClick}>Aceptar</Button>
         </DialogActions>
       </Dialog>
     </div>
