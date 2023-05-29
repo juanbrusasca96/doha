46d3e99418e4fe27ed443f05670ce0aa990a0c0c
Juan Brusasca <juanbrusasca96@gmail.com>
2023-05-29 16:51:46 -0300

    endDay modal

--- client/src/components/endDay/EndDay.jsx
+++ client/src/components/endDay/EndDay.jsx
@@ -21,22 +21,7 @@ export default function EndDay({ open, handleClose }) {
   return (
     <div>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
+        <DialogTitle>¿Seguro desea finalizar el dia?</DialogTitle>
         <DialogActions>
           <Button onClick={handleClose}>Cancelar</Button>
           <Button onClick={handleClick}>Aceptar</Button>
