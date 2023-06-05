1be4025fac91a26eb16e32134049576622a302c0
Juan Brusasca <juanbrusasca96@gmail.com>
2023-06-05 18:04:21 -0300

    quite react strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -15,11 +15,9 @@ axios.defaults.baseURL = process.env.REACT_APP_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
